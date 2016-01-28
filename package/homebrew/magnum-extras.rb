7a49d8683a046bce7d8d0d655a36de6a96facf78
Vladimír Vondruš <mosra@centrum.cz>
2016-01-28 15:51:25 +0100

    package: added Homebrew formula.

---
+++ package/homebrew/magnum-extras.rb
@@ -0,0 +1,19 @@
+# kate: indent-width 2;
+
+class MagnumExtras < Formula
+  desc "Extras for Magnum graphics engine"
+  homepage "https://github.com/mosra/magnum"
+  head "git://github.com/mosra/magnum-extras.git"
+
+  depends_on "cmake"
+  depends_on "magnum"
+
+  def install
+    system "mkdir build"
+    cd "build" do
+      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", ".."
+      system "cmake", "--build", "."
+      system "cmake", "--build", ".", "--target", "install"
+    end
+  end
+end
