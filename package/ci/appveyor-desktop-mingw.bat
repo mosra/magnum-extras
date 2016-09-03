eed34142994a8a732054d91526016d00402687c8
Vladimír Vondruš <mosra@centrum.cz>
2016-09-04 00:35:41 +0200

    package/ci: build for MinGW and Windows RT GLES2/GLES3 on AppVeyor.

---
+++ package/ci/appveyor-desktop-mingw.bat
@@ -0,0 +1,52 @@
+rem Workaround for CMake not wanting sh.exe on PATH for MinGW. AARGH.
+set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
+set PATH=C:\tools\mingw64\bin;%APPVEYOR_BUILD_FOLDER%\deps\bin;%PATH%
+
+rem Build Corrade. Could not get Ninja to work, meh.
+git clone --depth 1 git://github.com/mosra/corrade.git || exit /b
+cd corrade || exit /b
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DWITH_INTERCONNECT=OFF ^
+    -G "MinGW Makefiles" || exit /b
+cmake --build . -- -j || exit /b
+cmake --build . --target install -- -j || exit /b
+cd .. && cd ..
+
+rem Build Magnum
+git clone --depth 1 git://github.com/mosra/magnum.git || exit /b
+cd magnum || exit /b
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DWITH_AUDIO=OFF ^
+    -DWITH_DEBUGTOOLS=OFF ^
+    -DWITH_MESHTOOLS=OFF ^
+    -DWITH_PRIMITIVES=OFF ^
+    -DWITH_SCENEGRAPH=OFF ^
+    -DWITH_SHADERS=OFF ^
+    -DWITH_SHAPES=OFF ^
+    -DWITH_TEXT=OFF ^
+    -DWITH_TEXTURETOOLS=OFF ^
+    -DWITH_WINDOWLESSWGLAPPLICATION=ON ^
+    -G "MinGW Makefiles" || exit /b
+cmake --build . -- -j || exit /b
+cmake --build . --target install -- -j || exit /b
+cd .. && cd ..
+
+rem Build
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DBUILD_TESTS=ON ^
+    -DBUILD_GL_TESTS=ON ^
+    -G "MinGW Makefiles" || exit /b
+cmake --build . -- -j || exit /b
+cmake --build . --target install -- -j || exit /b
+
+rem Test
+ctest -V -E GLTest || exit /b
