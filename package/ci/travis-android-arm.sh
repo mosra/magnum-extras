e94b20037e12801df53f4c39302894ec2ae1e4ec
Vladimír Vondruš <mosra@centrum.cz>
2018-03-15 19:20:13 +0100

    package/ci: switch to new Android build workflow.

--- package/ci/travis-android-arm.sh
+++ package/ci/travis-android-arm.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -ev
 
-git submodule update --init
-
 git clone --depth 1 git://github.com/mosra/corrade.git
 cd corrade
 
@@ -21,10 +19,15 @@ cd ..
 
 # Crosscompile Corrade
 mkdir build-android-arm && cd build-android-arm
-ANDROID_NDK=$TRAVIS_BUILD_DIR/android-ndk-r10e cmake .. \
-    -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/Android-ARM.cmake \
-    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+cmake .. \
+    -DCMAKE_ANDROID_NDK=$TRAVIS_BUILD_DIR/android-ndk-r16b \
+    -DCMAKE_SYSTEM_NAME=Android \
+    -DCMAKE_SYSTEM_VERSION=22 \
+    -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a \
+    -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
+    -DCMAKE_ANDROID_STL_TYPE=c++_static \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
     -DCMAKE_INSTALL_PREFIX=$HOME/deps \
     -DWITH_INTERCONNECT=$TARGET_GLES3 \
     -G Ninja
@@ -35,10 +38,15 @@ cd ../..
 git clone --depth 1 git://github.com/mosra/magnum.git
 cd magnum
 mkdir build-android-arm && cd build-android-arm
-ANDROID_NDK=$TRAVIS_BUILD_DIR/android-ndk-r10e cmake .. \
-    -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/Android-ARM.cmake \
-    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+cmake .. \
+    -DCMAKE_ANDROID_NDK=$TRAVIS_BUILD_DIR/android-ndk-r16b \
+    -DCMAKE_SYSTEM_NAME=Android \
+    -DCMAKE_SYSTEM_VERSION=22 \
+    -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a \
+    -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
+    -DCMAKE_ANDROID_STL_TYPE=c++_static \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
     -DCMAKE_INSTALL_PREFIX=$HOME/deps \
     -DCMAKE_FIND_ROOT_PATH=$HOME/deps \
     -DWITH_AUDIO=OFF \
@@ -51,6 +59,7 @@ ANDROID_NDK=$TRAVIS_BUILD_DIR/android-ndk-r10e cmake .. \
     -DWITH_TEXT=$TARGET_GLES3 \
     -DWITH_TEXTURETOOLS=$TARGET_GLES3 \
     -DWITH_SDL2APPLICATION=OFF \
+    -DWITH_OPENGLTESTER=$TARGET_GLES3 \
     -DTARGET_GLES2=$TARGET_GLES2 \
     -G Ninja
 ninja install
@@ -59,21 +68,27 @@ cd ../..
 # Crosscompile. Not compiling the Ui gallery because I don't want to mess with
 # Android packaging just yet.
 mkdir build-android-arm && cd build-android-arm
-ANDROID_NDK=$TRAVIS_BUILD_DIR/android-ndk-r10e cmake .. \
-    -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/Android-ARM.cmake \
-    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+cmake .. \
+    -DCMAKE_ANDROID_NDK=$TRAVIS_BUILD_DIR/android-ndk-r16b \
+    -DCMAKE_SYSTEM_NAME=Android \
+    -DCMAKE_SYSTEM_VERSION=22 \
+    -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a \
+    -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
+    -DCMAKE_ANDROID_STL_TYPE=c++_static \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
     -DCMAKE_INSTALL_PREFIX=$HOME/deps \
     -DCMAKE_FIND_ROOT_PATH=$HOME/deps \
     -DWITH_UI=$TARGET_GLES3 \
     -DWITH_UI_GALLERY=OFF \
     -DBUILD_TESTS=ON \
+    -DBUILD_GL_TESTS=ON \
     -G Ninja
 # Otherwise the job gets killed (probably because using too much memory)
 ninja -j4
 
 # Start simulator and run tests
-echo no | android create avd --force -n test -t android-19 --abi armeabi-v7a
+echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a
 emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
-CORRADE_TEST_COLOR=ON ctest -V
+CORRADE_TEST_COLOR=ON ctest -V -E GLTest
