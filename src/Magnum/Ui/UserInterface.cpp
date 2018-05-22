36114c7abf78a8223142c15c22553e6c85ba8307
Vladimír Vondruš <mosra@centrum.cz>
2018-05-22 12:34:06 +0200

    Ui: use the new deprecation warning ignore macros.

--- src/Magnum/Ui/UserInterface.cpp
+++ src/Magnum/Ui/UserInterface.cpp
@@ -152,19 +152,9 @@ UserInterface::UserInterface(const Vector2& size, const Vector2i& screenSize, Te
     setStyleConfiguration(styleConfiguration);
 }
 
-#ifdef __GNUC__
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#elif defined(_MSC_VER)
-#pragma warning(push)
-#pragma warning(disable: 4996)
-#endif
+CORRADE_IGNORE_DEPRECATED_PUSH
 UserInterface::UserInterface(const Vector2& size, const Vector2i& screenSize, Text::AbstractFont& font): UserInterface{size, screenSize, font, defaultStyleConfiguration()} {}
-#ifdef __GNUC__
-#pragma GCC diagnostic pop
-#elif defined(_MSC_VER)
-#pragma warning(pop)
-#endif
+CORRADE_IGNORE_DEPRECATED_POP
 #endif
 
 UserInterface::~UserInterface() = default;
