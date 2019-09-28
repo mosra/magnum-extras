ab0f69b84c244aaaaa8601dc89834649e9b8dec7
Vladimír Vondruš <mosra@centrum.cz>
2019-09-28 20:42:32 +0200

    player: some formats (such as Basis or DDS) have more than one image.
    Support that.

--- src/player/Player.cpp
+++ src/player/Player.cpp
@@ -482,7 +482,7 @@ Player::Player(const Arguments& arguments): Platform::ScreenedApplication{argume
         /* If we passed a custom importer, try to figure out if it's an image
            or a scene */
         /** @todo ugh the importer should have an API for that */
-        if(args.value("importer") != "AnySceneImporter" && !importer->object3DCount() && !importer->mesh3DCount() && importer->image2DCount() == 1)
+        if(args.value("importer") != "AnySceneImporter" && !importer->object3DCount() && !importer->mesh3DCount() && importer->image2DCount() >= 1)
             _player = createImagePlayer(*this, *_overlay->ui);
         else
             _player = createScenePlayer(*this, *_overlay->ui);
