3e5f5d2caed4bcfbe4321c6625da78aba177e89c
Vladimír Vondruš <mosra@centrum.cz>
2020-10-23 18:39:13 +0200

    package/homebrew: update to what's actually latest.

--- package/homebrew/magnum-extras.rb
+++ package/homebrew/magnum-extras.rb
@@ -1,9 +1,9 @@
 class MagnumExtras < Formula
   desc "Extras for the Magnum C++11/C++14 graphics engine"
   homepage "https://magnum.graphics"
-  url "https://github.com/mosra/magnum-extras/archive/v2019.10.tar.gz"
-  # wget https://github.com/mosra/magnum-extras/archive/v2019.10.tar.gz -O - | sha256sum
-  sha256 "74d6d2800ef3755899af0f012e6de006fdffba77080215748edcf1911ac1c770"
+  url "https://github.com/mosra/magnum-extras/archive/v2020.06.tar.gz"
+  # wget https://github.com/mosra/magnum-extras/archive/v2020.06.tar.gz -O - | sha256sum
+  sha256 "a8d7babc50ac070984d39f6cc15c3ce2af7b41fe980fe81b0405da6f5ba3c36d"
   head "git://github.com/mosra/magnum-extras.git"
 
   depends_on "cmake"
@@ -12,7 +12,13 @@ class MagnumExtras < Formula
   def install
     system "mkdir build"
     cd "build" do
-      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DWITH_PLAYER=ON", "-DWITH_UI=ON", "-DWITH_UI_GALLERY=ON", ".."
+      system "cmake",
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+        "-DWITH_PLAYER=ON",
+        "-DWITH_UI=ON",
+        "-DWITH_UI_GALLERY=ON",
+        ".."
       system "cmake", "--build", "."
       system "cmake", "--build", ".", "--target", "install"
     end
