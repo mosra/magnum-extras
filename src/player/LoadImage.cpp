f42539902c97b731497137a36629cba053ceef96
Vladimír Vondruš <mosra@centrum.cz>
2023-05-16 21:23:07 +0200

    player: expand single- and two-channel images to RGB(A).
    The strided array views make the fallback bearable, but it should still
    eventually become something in TextureTools instead.

--- src/player/LoadImage.cpp
+++ src/player/LoadImage.cpp
@@ -25,9 +25,13 @@
 
 #include "LoadImage.h"
 
+#include <Corrade/Containers/StridedArrayView.h>
+#include <Corrade/Utility/Algorithms.h>
 #include <Magnum/ImageView.h>
 #include <Magnum/PixelFormat.h>
 #include <Magnum/Math/Functions.h>
+#include <Magnum/GL/Context.h>
+#include <Magnum/GL/Extensions.h>
 #include <Magnum/GL/Texture.h>
 #include <Magnum/GL/TextureFormat.h>
 #include <Magnum/Trade/ImageData.h>
@@ -36,10 +40,67 @@ namespace Magnum { namespace Player {
 
 void loadImage(GL::Texture2D& texture, const Trade::ImageData2D& image) {
     if(!image.isCompressed()) {
+        /* Single-channel images are probably meant to represent grayscale,
+           two-channel grayscale + alpha. Probably, there's no way to know, but
+           given we're using them for *colors*, it makes more sense than
+           displaying them just red or red+green. */
+        Containers::Array<char> usedImageStorage;
+        ImageView2D usedImage = image;
+        const UnsignedInt channelCount = pixelFormatChannelCount(image.format());
+        if(channelCount == 1 || channelCount == 2) {
+            #ifndef MAGNUM_TARGET_WEBGL
+            #ifndef MAGNUM_TARGET_GLES
+            /* Available in GLES 3 always */
+            if(GL::Context::current().isExtensionSupported<GL::Extensions::ARB::texture_swizzle>())
+            #endif
+            {
+                if(channelCount == 1)
+                    texture.setSwizzle<'r', 'r', 'r', '1'>();
+                else if(channelCount == 2)
+                    texture.setSwizzle<'r', 'r', 'r', 'g'>();
+            }
+            #ifndef MAGNUM_TARGET_GLES
+            else
+            #endif
+            #endif
+            {
+                /* Without texture swizzle support, allocate a copy of the image
+                   and expand the channels manually */
+                /** @todo make this a utility in TextureTools, with the channel
+                    expansion being an optimized routine in Math/PackingBatch.h */
+                const PixelFormat imageFormat = pixelFormat(image.format(), channelCount == 2 ? 4 : 3, isPixelFormatSrgb(image.format()));
+                Debug{} << "Texture swizzle not supported, expanding a" << image.format() << "image to" << imageFormat;
+
+                /* Pad to four-byte rows to not have to use non-optimal
+                   alignment */
+                const std::size_t rowStride = 4*((pixelFormatSize(imageFormat)*image.size().x() + 3)/4);
+                usedImageStorage = Containers::Array<char>{NoInit, std::size_t(rowStride*image.size().y())};
+                const MutableImageView2D usedMutableImage{imageFormat, image.size(), usedImageStorage};
+                usedImage = usedMutableImage;
+
+                /* Create 4D pixel views (rows, pixels, channels, channel
+                   bytes) */
+                const std::size_t channelSize = pixelFormatSize(pixelFormatChannelFormat(imageFormat));
+                const std::size_t dstChannelCount = pixelFormatChannelCount(imageFormat);
+                const Containers::StridedArrayView4D<const char> src = image.pixels().expanded<2>(Containers::Size2D{channelCount, channelSize});
+                const Containers::StridedArrayView4D<char> dst = usedMutableImage.pixels().expanded<2>(Containers::Size2D{dstChannelCount, channelSize});
+
+                /* Broadcast the red channel of the input to RRR and copy to
+                   the RGB channels of the output */
+                Utility::copy(
+                    src.exceptSuffix({0, 0, channelCount == 2 ? 1 : 0, 0}).broadcasted<2>(3),
+                    dst.exceptSuffix({0, 0, channelCount == 2 ? 1 : 0, 0}));
+                /* If there's an alpha channel, copy it over as well */
+                if(channelCount == 2) Utility::copy(
+                    src.exceptPrefix({0, 0, 1, 0}),
+                    dst.exceptPrefix({0, 0, 3, 0}));
+            }
+        }
+
         /* Whitelist only things we *can* display */
         /** @todo signed formats, exposure knob for float formats */
         GL::TextureFormat format;
-        switch(image.format()) {
+        switch(usedImage.format()) {
             case PixelFormat::R8Unorm:
             case PixelFormat::RG8Unorm:
             /* can't really do sRGB R/RG as there are no widely available
@@ -63,16 +124,16 @@ void loadImage(GL::Texture2D& texture, const Trade::ImageData2D& image) {
             case PixelFormat::RG32F:
             case PixelFormat::RGB32F:
             case PixelFormat::RGBA32F:
-                format = GL::textureFormat(image.format());
+                format = GL::textureFormat(usedImage.format());
                 break;
             default:
-                Warning{} << "Cannot load an image of format" << image.format();
+                Warning{} << "Cannot load an image of format" << usedImage.format();
                 return;
         }
 
         texture
-            .setStorage(Math::log2(image.size().max()) + 1, format, image.size())
-            .setSubImage(0, {}, image)
+            .setStorage(Math::log2(usedImage.size().max()) + 1, format, usedImage.size())
+            .setSubImage(0, {}, usedImage)
             .generateMipmap();
 
     } else {
