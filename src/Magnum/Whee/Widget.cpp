f028ac3cbea964b34030d9c5a0ba2e51c3d673a0
Vladimír Vondruš <mosra@centrum.cz>
2023-11-12 00:28:08 +0100

    Whee: add a base Widget class.
    RAII wrapper over a node, meant for stateful widgets, i.e. when it's
    desirable to be able to change the state after, such as modifying a text
    label or button color, or for example for widgets that will themselves
    have some persistent data, such as text input, list views and such.
    
    Originally I thought that I would not make it own the node, thus the
    widget would stay in the UI even after the instance is gone. That would
    allow for a use case where the UI is populated simply by constructing
    widget instances, and then only the instances which need to be accessed
    / modified after will stay in scope, the rest will simply disappear.
    That would however be efficient only for some cases, and heavier widgets
    would waste time constructing (or worse, allocating) state in
    anticipation of it being accessed later but then immediately calling
    destructors on it because it won't. And it'd also break the usual RAII
    expectations, where (unlike with any other Magnum API) one has to
    explicitly do something in order to actually delete the widget at the
    end of a scope.
    
    So it's by default owning, and to avoid the nastiness with cascading
    destructors, I plan to have stateless variants of widgets for which it
    makes sense. For example:
    
        Button stateful{ui, ..., "Press me"};
        ui.eventLayer().onTapOrClick(stateful, ...);
    
        Whee::NodeHandle stateless = Whee::button(ui, ..., "Press me");
        ui.eventLayer().onTapOrClick(stateless, ...);
    
    Both lines above do the same, except that the `stateful` instance can
    subsequently change the text and such, for which the user will pay a
    destructor cost. The `stateless` gets automatically (and efficiently)
    removed by the UI itself when the parent node is gone (or explicitly
    (and still efficiently) by removing the node handle), until then it will
    be alive, react to events etc. as usual.
    
    Originally the Widget also allowed itself to be constructed from an
    invalid node handle, and ignored an invalid node handle on destruction.
    While that seemed to make sense originally, it could cause certain kind
    of bugs such as incorrect destruction order to go unnoticed, so I assert
    on that now as well.

---
+++ src/Magnum/Whee/Widget.cpp
@@ -0,0 +1,84 @@
+/*
+    This file is part of Magnum.
+
+    Copyright © 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
+                2020, 2021, 2022, 2023 Vladimír Vondruš <mosra@centrum.cz>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#include "Widget.h"
+
+#include <Corrade/Utility/Assert.h>
+
+#include "Magnum/Whee/Handle.h"
+#include "Magnum/Whee/NodeFlags.h"
+#include "Magnum/Whee/AbstractUserInterface.h"
+
+namespace Magnum { namespace Whee {
+
+AbstractWidget::AbstractWidget(AbstractUserInterface& ui, NodeHandle node): _ui{ui}, _node{node} {
+    CORRADE_ASSERT(ui.isHandleValid(node),
+        "Whee::AbstractWidget: invalid handle" << node, );
+}
+
+AbstractWidget::AbstractWidget(AbstractWidget&& other) noexcept: _ui{other._ui}, _node{other._node} {
+    /* not using NodeHandle::Null to not need to include Handle.h */
+    other._node = NodeHandle{};
+}
+
+AbstractWidget::~AbstractWidget() {
+    if(_node != NodeHandle::Null) {
+        CORRADE_ASSERT(_ui->isHandleValid(_node),
+            "Whee::AbstractWidget: invalid handle" << _node << "on destruction", );
+        _ui->removeNode(_node);
+    }
+}
+
+AbstractWidget& AbstractWidget::operator=(AbstractWidget&& other) noexcept {
+    Utility::swap(_ui, other._ui);
+    Utility::swap(_node, other._node);
+    return *this;
+}
+
+bool AbstractWidget::isHidden() const {
+    return _ui->nodeFlags(_node) >= NodeFlag::Hidden;
+}
+
+void AbstractWidget::setHidden(const bool hidden) {
+    hidden ? _ui->addNodeFlags(_node, NodeFlag::Hidden) :
+             _ui->clearNodeFlags(_node, NodeFlag::Hidden);
+}
+
+bool AbstractWidget::isDisabled() const {
+    return _ui->nodeFlags(_node) >= NodeFlag::Disabled;
+}
+
+void AbstractWidget::setDisabled(const bool disabled) {
+    disabled ? _ui->addNodeFlags(_node, NodeFlag::Disabled) :
+               _ui->clearNodeFlags(_node, NodeFlag::Disabled);
+}
+
+NodeHandle AbstractWidget::release() {
+    const NodeHandle out = _node;
+    _node = NodeHandle::Null;
+    return out;
+}
+
+}}
