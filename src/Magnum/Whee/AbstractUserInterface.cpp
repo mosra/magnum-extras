3aa5b1e46e0ab7bb1bddbe1ad80319072cb85c33
Vladimír Vondruš <mosra@centrum.cz>
2023-12-03 22:59:05 +0100

    Whee: split draw and event data ordering.
    Right now in isolation the change doesn't make sense as the operation of
    counting data attached to visible nodes is now performed twice for
    seemingly no reason. But that will change with the introduction of nodes
    with disabled events, as in that case the masks will differ between the
    two.

--- src/Magnum/Whee/AbstractUserInterface.cpp
+++ src/Magnum/Whee/AbstractUserInterface.cpp
@@ -1908,7 +1908,6 @@ AbstractUserInterface& AbstractUserInterface::update() {
                         state.visibleNodeMask,
                         state.clipRectNodeCounts.prefix(state.clipRectCount),
                         visibleNodeDataOffsets,
-                        state.visibleNodeEventDataOffsets.exceptPrefix(1),
                         visibleNodeDataIds.prefix(instance->capacity()),
                         state.dataToUpdateIds,
                         state.dataToUpdateClipRectIds,
@@ -1948,6 +1947,14 @@ AbstractUserInterface& AbstractUserInterface::update() {
                         )
                             j = i;
                     }
+
+                    /* If the layer has LayerFeature::Event, count the data for
+                       it, accumulating them across all event layers */
+                    if(layerItem.used.features >= LayerFeature::Event)
+                        Implementation::countNodeDataForEventHandlingInto(
+                            instance->nodes(),
+                            state.visibleNodeEventDataOffsets,
+                            state.visibleNodeMask);
                 }
 
                 state.dataToUpdateLayerOffsets[i + 1] = {offset, clipRectOffset};
