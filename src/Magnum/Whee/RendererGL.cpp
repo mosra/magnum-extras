1d8cf30487b47889d2c5c0aab0d242e38c9c4f82
Vladimír Vondruš <mosra@centrum.cz>
2024-01-08 21:42:35 +0100

    Whee: make RendererGL provide an optional compositing framebuffer.
    For use by layers that implement compositing.

--- src/Magnum/Whee/RendererGL.cpp
+++ src/Magnum/Whee/RendererGL.cpp
@@ -25,16 +25,46 @@
 
 #include "RendererGL.h"
 
+#include <Corrade/Containers/EnumSet.hpp>
+#include <Corrade/Utility/Assert.h>
+#include <Magnum/GL/Framebuffer.h>
 #include <Magnum/GL/Renderer.h>
+#include <Magnum/GL/Texture.h>
+#include <Magnum/GL/TextureFormat.h>
 #include <Magnum/Math/Range.h>
 
 namespace Magnum { namespace Whee {
 
+Debug& operator<<(Debug& debug, const RendererGL::Flag value) {
+    debug << "Whee::RendererGL::Flag" << Debug::nospace;
+
+    switch(value) {
+        /* LCOV_EXCL_START */
+        #define _c(value) case RendererGL::Flag::value: return debug << "::" #value;
+        _c(CompositingFramebuffer)
+        #undef _c
+        /* LCOV_EXCL_STOP */
+    }
+
+    return debug << "(" << Debug::nospace << Debug::hex << UnsignedByte(value) << Debug::nospace << ")";
+}
+
+Debug& operator<<(Debug& debug, const RendererGL::Flags value) {
+    return Containers::enumSetDebugOutput(debug, value, "Whee::RendererGL::Flags{}", {
+        RendererGL::Flag::CompositingFramebuffer
+    });
+}
+
 struct RendererGL::State {
+    explicit State(Flags flags): flags{flags} {}
+
     bool scissorUsed = false;
+    Flags flags;
+    GL::Texture2D compositingTexture{NoCreate};
+    GL::Framebuffer compositingFramebuffer{NoCreate};
 };
 
-RendererGL::RendererGL(): _state{InPlaceInit} {}
+RendererGL::RendererGL(const Flags flags): _state{InPlaceInit, flags} {}
 
 RendererGL::RendererGL(RendererGL&&) noexcept = default;
 
@@ -42,13 +72,69 @@ RendererGL::~RendererGL() = default;
 
 RendererGL& RendererGL::operator=(RendererGL&&) noexcept = default;
 
-RendererFeatures RendererGL::doFeatures() const { return {}; }
+RendererGL::Flags RendererGL::flags() const { return _state->flags; }
 
-void RendererGL::doSetupFramebuffers(const Vector2i&) {}
+const GL::Framebuffer& RendererGL::compositingFramebuffer() const {
+    const State& state = *_state;
+    CORRADE_ASSERT(state.flags & Flag::CompositingFramebuffer,
+        "Whee::RendererGL::compositingFramebuffer(): compositing framebuffer not enabled", state.compositingFramebuffer);
+    CORRADE_ASSERT(!framebufferSize().isZero(),
+        "Whee::RendererGL::compositingFramebuffer(): framebuffer size wasn't set up", state.compositingFramebuffer);
+    return state.compositingFramebuffer;
+}
+
+GL::Framebuffer& RendererGL::compositingFramebuffer() {
+    return const_cast<GL::Framebuffer&>(const_cast<const RendererGL&>(*this).compositingFramebuffer());
+}
+
+const GL::Texture2D& RendererGL::compositingTexture() const {
+    const State& state = *_state;
+    CORRADE_ASSERT(state.flags & Flag::CompositingFramebuffer,
+        "Whee::RendererGL::compositingTexture(): compositing framebuffer not enabled", state.compositingTexture);
+    CORRADE_ASSERT(!framebufferSize().isZero(),
+        "Whee::RendererGL::compositingTexture(): framebuffer size wasn't set up", state.compositingTexture);
+    return state.compositingTexture;
+}
+
+GL::Texture2D& RendererGL::compositingTexture() {
+    return const_cast<GL::Texture2D&>(const_cast<const RendererGL&>(*this).compositingTexture());
+}
+
+RendererFeatures RendererGL::doFeatures() const {
+    return _state->flags & Flag::CompositingFramebuffer ?
+        RendererFeature::Composite : RendererFeatures{};
+}
+
+void RendererGL::doSetupFramebuffers(const Vector2i& size) {
+    /** @todo recreate only if size changes, and not if size gets smaller?
+        would however mean the compositor needs to be aware that there's just a
+        subset of the texture being used */
+    if(_state->flags & Flag::CompositingFramebuffer) {
+        (_state->compositingTexture = GL::Texture2D{})
+            .setMinificationFilter(GL::SamplerFilter::Linear, GL::SamplerMipmap::Base)
+            .setMagnificationFilter(GL::SamplerFilter::Linear)
+            .setWrapping(GL::SamplerWrapping::ClampToEdge)
+            .setStorage(1, GL::TextureFormat::RGBA8, size);
+        (_state->compositingFramebuffer = GL::Framebuffer{{{}, size}})
+            .attachTexture(GL::Framebuffer::ColorAttachment{0}, _state->compositingTexture, 0);
+    }
+}
 
 void RendererGL::doTransition(RendererTargetState, const RendererTargetState targetStateTo, const RendererDrawStates drawStatesFrom, const RendererDrawStates drawStatesTo) {
     State& state = *_state;
 
+    /* If the compositing framebuffer is active, make sure to bind it when
+       transitioning to a layer draw state or to the final state. */
+    if(state.flags & Flag::CompositingFramebuffer &&
+       (targetStateTo == RendererTargetState::Draw ||
+        targetStateTo == RendererTargetState::Final))
+    {
+        state.compositingFramebuffer.bind();
+    }
+
+    /* Flip GL state as appropriate. This does the right thing (i.e., disabling
+       both) for compositing transition as well, which is enforced by the base
+       class. */
     if((drawStatesFrom >= RendererDrawState::Blending) !=
          (drawStatesTo >= RendererDrawState::Blending)) {
         GL::Renderer::setFeature(GL::Renderer::Feature::Blending, drawStatesTo >= RendererDrawState::Blending);
