e0f4d9d5e07534963ce2689b6045239c2283f18d
Vladimír Vondruš <mosra@centrum.cz>
2024-01-08 21:42:35 +0100

    Whee: make RendererGL provide an optional compositing framebuffer.
    For use by layers that implement compositing.

--- src/Magnum/Whee/Test/RendererGLTest.cpp
+++ src/Magnum/Whee/Test/RendererGLTest.cpp
@@ -23,7 +23,13 @@
     DEALINGS IN THE SOFTWARE.
 */
 
+#include <sstream> /** @todo remove once Debug is stream-free */
+#include <Corrade/Containers/StringStl.h> /** @todo remove once Debug is stream-free */
+#include <Corrade/TestSuite/Compare/String.h>
+#include <Magnum/GL/Framebuffer.h>
+#include <Magnum/GL/DefaultFramebuffer.h>
 #include <Magnum/GL/OpenGLTester.h>
+#include <Magnum/GL/Texture.h>
 #include <Magnum/Math/Vector4.h>
 
 #include "Magnum/Whee/RendererGL.h"
@@ -34,21 +40,31 @@ struct RendererGLTest: GL::OpenGLTester {
     explicit RendererGLTest();
 
     void construct();
+    void constructCompositingFramebuffer();
     void constructCopy();
     void constructMove();
+
+    void compositingFramebuffer();
+    void compositingFramebufferNoFramebufferSizeSet();
 
     void setupTeardown();
 
     void transition();
+    void transitionCompositing();
     void transitionNoScissor();
 };
 
 RendererGLTest::RendererGLTest() {
     addTests({&RendererGLTest::construct,
+              &RendererGLTest::constructCompositingFramebuffer,
               &RendererGLTest::constructCopy,
-              &RendererGLTest::constructMove});
+              &RendererGLTest::constructMove,
+
+              &RendererGLTest::compositingFramebuffer,
+              &RendererGLTest::compositingFramebufferNoFramebufferSizeSet});
 
     addTests({&RendererGLTest::transition,
+              &RendererGLTest::transitionCompositing,
               &RendererGLTest::transitionNoScissor},
               &RendererGLTest::setupTeardown,
               &RendererGLTest::setupTeardown);
@@ -56,9 +72,16 @@ RendererGLTest::RendererGLTest() {
 
 void RendererGLTest::construct() {
     RendererGL renderer;
+    CORRADE_COMPARE(renderer.flags(), RendererGL::Flags{});
+    CORRADE_COMPARE(renderer.features(), RendererFeatures{});
+}
 
-    /* There's nothing else to query on top of AbstractRenderer */
-    CORRADE_COMPARE(renderer.currentTargetState(), RendererTargetState::Initial);
+void RendererGLTest::constructCompositingFramebuffer() {
+    RendererGL renderer{RendererGL::Flag::CompositingFramebuffer};
+    CORRADE_COMPARE(renderer.flags(), RendererGL::Flag::CompositingFramebuffer);
+    CORRADE_COMPARE(renderer.features(), RendererFeature::Composite);
+
+    /* Queries tested in compositingFramebuffer() as they need also size set */
 }
 
 void RendererGLTest::constructCopy() {
@@ -67,25 +90,72 @@ void RendererGLTest::constructCopy() {
 }
 
 void RendererGLTest::constructMove() {
-    RendererGL a;
+    RendererGL a{RendererGL::Flag::CompositingFramebuffer};
     a.setupFramebuffers({15, 37});
 
     RendererGL b = Utility::move(a);
-    /* There's nothing else to query on top of AbstractRenderer */
+    CORRADE_COMPARE(b.flags(), RendererGL::Flag::CompositingFramebuffer);
     CORRADE_COMPARE(b.framebufferSize(), (Vector2i{15, 37}));
 
     RendererGL c;
     c = Utility::move(b);
-    /* There's nothing else to query on top of AbstractRenderer */
+    CORRADE_COMPARE(c.flags(), RendererGL::Flag::CompositingFramebuffer);
     CORRADE_COMPARE(c.framebufferSize(), (Vector2i{15, 37}));
 
     CORRADE_VERIFY(std::is_nothrow_move_constructible<RendererGL>::value);
     CORRADE_VERIFY(std::is_nothrow_move_assignable<RendererGL>::value);
 }
 
+void RendererGLTest::compositingFramebuffer() {
+    RendererGL renderer{RendererGL::Flag::CompositingFramebuffer};
+    const RendererGL& crenderer = renderer;
+
+    /* The objects are created on the first framebuffer size setup */
+    renderer.setupFramebuffers({200, 300});
+    UnsignedInt framebufferId = renderer.compositingFramebuffer().id();
+    UnsignedInt textureId = renderer.compositingTexture().id();
+    MAGNUM_VERIFY_NO_GL_ERROR();
+    CORRADE_VERIFY(framebufferId);
+    CORRADE_COMPARE(crenderer.compositingFramebuffer().id(), framebufferId);
+    CORRADE_COMPARE(renderer.compositingFramebuffer().viewport(), (Range2Di{{}, {200, 300}}));
+    CORRADE_VERIFY(textureId);
+    CORRADE_COMPARE(crenderer.compositingTexture().id(), textureId);
+    /* Nothing else to verify on the texture */
+
+    /* They get recreated from scratch on resize */
+    renderer.setupFramebuffers({150, 200});
+    MAGNUM_VERIFY_NO_GL_ERROR();
+    CORRADE_VERIFY(renderer.compositingFramebuffer().id());
+    CORRADE_VERIFY(renderer.compositingFramebuffer().id() != framebufferId);
+    CORRADE_COMPARE(renderer.compositingFramebuffer().viewport(), (Range2Di{{}, {150, 200}}));
+    CORRADE_VERIFY(renderer.compositingTexture().id());
+    CORRADE_VERIFY(renderer.compositingTexture().id() != textureId);
+}
+
+void RendererGLTest::compositingFramebufferNoFramebufferSizeSet() {
+    CORRADE_SKIP_IF_NO_ASSERT();
+
+    RendererGL renderer{RendererGL::Flag::CompositingFramebuffer};
+    const RendererGL& crenderer = renderer;
+
+    std::ostringstream out;
+    Error redirectError{&out};
+    renderer.compositingFramebuffer();
+    crenderer.compositingFramebuffer();
+    renderer.compositingTexture();
+    crenderer.compositingTexture();
+    CORRADE_COMPARE_AS(out.str(),
+        "Whee::RendererGL::compositingFramebuffer(): framebuffer size wasn't set up\n"
+        "Whee::RendererGL::compositingFramebuffer(): framebuffer size wasn't set up\n"
+        "Whee::RendererGL::compositingTexture(): framebuffer size wasn't set up\n"
+        "Whee::RendererGL::compositingTexture(): framebuffer size wasn't set up\n",
+        TestSuite::Compare::String);
+}
+
 void RendererGLTest::setupTeardown() {
     GL::Renderer::disable(GL::Renderer::Feature::Blending);
     GL::Renderer::disable(GL::Renderer::Feature::ScissorTest);
+    GL::defaultFramebuffer.bind();
 }
 
 void RendererGLTest::transition() {
@@ -93,7 +163,7 @@ void RendererGLTest::transition() {
     glGetIntegerv(GL_SCISSOR_BOX, defaultScissorRect.data());
     MAGNUM_VERIFY_NO_GL_ERROR();
 
-    Int currentBlending = 1, currentScissor = 1;
+    Int currentBlending = 1, currentScissor = 1, currentFramebuffer = 999;
     Vector4i currentScissorRect{};
 
     RendererGL renderer;
@@ -102,10 +172,14 @@ void RendererGLTest::transition() {
     glGetIntegerv(GL_BLEND, &currentBlending);
     glGetIntegerv(GL_SCISSOR_TEST, &currentScissor);
     glGetIntegerv(GL_SCISSOR_BOX, currentScissorRect.data());
+    glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &currentFramebuffer);
     MAGNUM_VERIFY_NO_GL_ERROR();
     CORRADE_VERIFY(!currentBlending);
     CORRADE_VERIFY(!currentScissor);
     CORRADE_COMPARE(currentScissorRect, defaultScissorRect);
+    /* Currently bound framebuffer should not be changed at all during the
+       whole lifetime */
+    CORRADE_COMPARE(currentFramebuffer, 0);
 
     glScissor(0, 1, 2, 3);
 
@@ -113,47 +187,106 @@ void RendererGLTest::transition() {
     glGetIntegerv(GL_BLEND, &currentBlending);
     glGetIntegerv(GL_SCISSOR_TEST, &currentScissor);
     glGetIntegerv(GL_SCISSOR_BOX, currentScissorRect.data());
+    glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &currentFramebuffer);
     MAGNUM_VERIFY_NO_GL_ERROR();
     CORRADE_VERIFY(currentBlending);
     CORRADE_VERIFY(!currentScissor);
     CORRADE_COMPARE(currentScissorRect, (Vector4i{0, 1, 2, 3}));
+    CORRADE_COMPARE(currentFramebuffer, 0);
 
     renderer.transition(RendererTargetState::Draw, RendererDrawState::Scissor);
     glGetIntegerv(GL_BLEND, &currentBlending);
     glGetIntegerv(GL_SCISSOR_TEST, &currentScissor);
     glGetIntegerv(GL_SCISSOR_BOX, currentScissorRect.data());
+    glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &currentFramebuffer);
     MAGNUM_VERIFY_NO_GL_ERROR();
     CORRADE_VERIFY(!currentBlending);
     CORRADE_VERIFY(currentScissor);
     CORRADE_COMPARE(currentScissorRect, (Vector4i{0, 1, 2, 3}));
+    CORRADE_COMPARE(currentFramebuffer, 0);
 
     renderer.transition(RendererTargetState::Draw, RendererDrawState::Blending|RendererDrawState::Scissor);
     glGetIntegerv(GL_BLEND, &currentBlending);
     glGetIntegerv(GL_SCISSOR_TEST, &currentScissor);
     glGetIntegerv(GL_SCISSOR_BOX, currentScissorRect.data());
+    glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &currentFramebuffer);
     MAGNUM_VERIFY_NO_GL_ERROR();
     CORRADE_VERIFY(currentBlending);
     CORRADE_VERIFY(currentScissor);
     CORRADE_COMPARE(currentScissorRect, (Vector4i{0, 1, 2, 3}));
+    CORRADE_COMPARE(currentFramebuffer, 0);
 
     renderer.transition(RendererTargetState::Draw, {});
     glGetIntegerv(GL_BLEND, &currentBlending);
     glGetIntegerv(GL_SCISSOR_TEST, &currentScissor);
     glGetIntegerv(GL_SCISSOR_BOX, currentScissorRect.data());
+    glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &currentFramebuffer);
     MAGNUM_VERIFY_NO_GL_ERROR();
     CORRADE_VERIFY(!currentBlending);
     CORRADE_VERIFY(!currentScissor);
     CORRADE_COMPARE(currentScissorRect, (Vector4i{0, 1, 2, 3}));
+    CORRADE_COMPARE(currentFramebuffer, 0);
 
     renderer.transition(RendererTargetState::Final, {});
     glGetIntegerv(GL_BLEND, &currentBlending);
     glGetIntegerv(GL_SCISSOR_TEST, &currentScissor);
     glGetIntegerv(GL_SCISSOR_BOX, currentScissorRect.data());
+    glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &currentFramebuffer);
     MAGNUM_VERIFY_NO_GL_ERROR();
     CORRADE_VERIFY(!currentBlending);
     CORRADE_VERIFY(!currentScissor);
     /* Scissor rectangle should get reset to the full framebuffer size */
     CORRADE_COMPARE(currentScissorRect, (Vector4i{0, 0, 15, 37}));
+    CORRADE_COMPARE(currentFramebuffer, 0);
+}
+
+void RendererGLTest::transitionCompositing() {
+    Int currentFramebuffer = 999;
+
+    GL::Framebuffer anotherFramebuffer{{}};
+
+    RendererGL renderer{RendererGL::Flag::CompositingFramebuffer};
+    renderer.setupFramebuffers({15, 37});
+
+    /* Setting up the renderer should not bind the framebuffer yet */
+    glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &currentFramebuffer);
+    MAGNUM_VERIFY_NO_GL_ERROR();
+    CORRADE_COMPARE(currentFramebuffer, 0);
+
+    /* Transitioning to the Initial state does nothing */
+    renderer.transition(RendererTargetState::Initial, {});
+    glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &currentFramebuffer);
+    MAGNUM_VERIFY_NO_GL_ERROR();
+    CORRADE_COMPARE(currentFramebuffer, 0);
+
+    /* Transitioning to a Draw state will bind it */
+    anotherFramebuffer.bind();
+    renderer.transition(RendererTargetState::Draw, {});
+    glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &currentFramebuffer);
+    MAGNUM_VERIFY_NO_GL_ERROR();
+    CORRADE_COMPARE(currentFramebuffer, renderer.compositingFramebuffer().id());
+
+    /* Transitioning to a Composite state will not bind it */
+    anotherFramebuffer.bind();
+    renderer.transition(RendererTargetState::Composite, {});
+    glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &currentFramebuffer);
+    MAGNUM_VERIFY_NO_GL_ERROR();
+    CORRADE_COMPARE(currentFramebuffer, anotherFramebuffer.id());
+
+    /* Transitioning to a Draw state will bind it back */
+    renderer.transition(RendererTargetState::Draw, {});
+    glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &currentFramebuffer);
+    MAGNUM_VERIFY_NO_GL_ERROR();
+    CORRADE_COMPARE(currentFramebuffer, renderer.compositingFramebuffer().id());
+
+    /* Transitioning to a Final state will rebind it again. Which shouldn't be
+       needed in practice, but the state tracker will deal with that
+       redundancy. */
+    anotherFramebuffer.bind();
+    renderer.transition(RendererTargetState::Final, {});
+    glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &currentFramebuffer);
+    MAGNUM_VERIFY_NO_GL_ERROR();
+    CORRADE_COMPARE(currentFramebuffer, renderer.compositingFramebuffer().id());
 }
 
 void RendererGLTest::transitionNoScissor() {
--- src/Magnum/Whee/Test/RendererGL_Test.cpp
+++ src/Magnum/Whee/Test/RendererGL_Test.cpp
@@ -23,7 +23,11 @@
     DEALINGS IN THE SOFTWARE.
 */
 
+#include <sstream> /** @todo remove once Debug is stream-free */
+#include <Corrade/Containers/StringStl.h> /** @todo remove once Debug is stream-free */
 #include <Corrade/TestSuite/Tester.h>
+#include <Corrade/TestSuite/Compare/String.h>
+#include <Corrade/Utility/DebugStl.h> /** @todo remove once Debug is stream-free */
 
 #include "Magnum/Whee/RendererGL.h"
 
@@ -32,11 +36,33 @@ namespace Magnum { namespace Whee { namespace Test { namespace {
 struct RendererGL_Test: TestSuite::Tester {
     explicit RendererGL_Test();
 
+    void debugFlag();
+    void debugFlags();
+
     void construct();
+
+    void compositingFramebufferTextureNotEnabled();
 };
 
 RendererGL_Test::RendererGL_Test() {
-    addTests({&RendererGL_Test::construct});
+    addTests({&RendererGL_Test::debugFlag,
+              &RendererGL_Test::debugFlags,
+
+              &RendererGL_Test::construct,
+
+              &RendererGL_Test::compositingFramebufferTextureNotEnabled});
+}
+
+void RendererGL_Test::debugFlag() {
+    std::ostringstream out;
+    Debug{&out} << RendererGL::Flag::CompositingFramebuffer << RendererGL::Flag(0xbe);
+    CORRADE_COMPARE(out.str(), "Whee::RendererGL::Flag::CompositingFramebuffer Whee::RendererGL::Flag(0xbe)\n");
+}
+
+void RendererGL_Test::debugFlags() {
+    std::ostringstream out;
+    Debug{&out} << (RendererGL::Flag::CompositingFramebuffer|RendererGL::Flag(0xb0)) << RendererGL::Flags{};
+    CORRADE_COMPARE(out.str(), "Whee::RendererGL::Flag::CompositingFramebuffer|Whee::RendererGL::Flag(0xb0) Whee::RendererGL::Flags{}\n");
 }
 
 void RendererGL_Test::construct() {
@@ -44,6 +70,26 @@ void RendererGL_Test::construct() {
 
     /* It shouldn't require a GL context on construction or destruction */
     CORRADE_COMPARE(renderer.currentDrawStates(), RendererDrawStates{});
+}
+
+void RendererGL_Test::compositingFramebufferTextureNotEnabled() {
+    CORRADE_SKIP_IF_NO_ASSERT();
+
+    RendererGL renderer;
+    const RendererGL& crenderer = renderer;
+
+    std::ostringstream out;
+    Error redirectError{&out};
+    renderer.compositingFramebuffer();
+    crenderer.compositingFramebuffer();
+    renderer.compositingTexture();
+    crenderer.compositingTexture();
+    CORRADE_COMPARE_AS(out.str(),
+        "Whee::RendererGL::compositingFramebuffer(): compositing framebuffer not enabled\n"
+        "Whee::RendererGL::compositingFramebuffer(): compositing framebuffer not enabled\n"
+        "Whee::RendererGL::compositingTexture(): compositing framebuffer not enabled\n"
+        "Whee::RendererGL::compositingTexture(): compositing framebuffer not enabled\n",
+        TestSuite::Compare::String);
 }
 
 }}}}
