b2a0e4f2fe51fc8acfdab2895b7e6d6feace5167
Vladimír Vondruš <mosra@centrum.cz>
2024-03-08 12:56:50 +0100

    Whee: wire dynamic style count into AbstractStyle.
    Together with an ability to override the count wanted by the style,
    similarly to how it's possible to do for text layer glyph cache size.
    For example if given style doesn't do any animations on its own, it
    won't even implement do*LayerDynamicStyleCount(). But the application
    might, in which case it just calls set*LayerDynamicStyleCount() when
    applying the style.

--- src/Magnum/Whee/UserInterfaceGL.cpp
+++ src/Magnum/Whee/UserInterfaceGL.cpp
@@ -96,7 +96,8 @@ bool UserInterfaceGL::trySetStyle(const AbstractStyle& style, const StyleFeature
             "Whee::UserInterfaceGL::trySetStyle(): base layer already present", {});
         state.baseLayerShared = BaseLayerGL::Shared{
             BaseLayer::Shared::Configuration{style.baseLayerStyleUniformCount(),
-                                             style.baseLayerStyleCount()}};
+                                             style.baseLayerStyleCount()}
+                .setDynamicStyleCount(style.baseLayerDynamicStyleCount())};
         setBaseLayerInstance(Containers::pointer<BaseLayerGL>(createLayer(), state.baseLayerShared));
     }
     if(features >= StyleFeature::TextLayer) {
@@ -104,7 +105,8 @@ bool UserInterfaceGL::trySetStyle(const AbstractStyle& style, const StyleFeature
             "Whee::UserInterfaceGL::trySetStyle(): text layer already present", {});
         state.textLayerShared = TextLayerGL::Shared{
             TextLayer::Shared::Configuration{style.textLayerStyleUniformCount(),
-                                             style.textLayerStyleCount()}};
+                                             style.textLayerStyleCount()}
+                .setDynamicStyleCount(style.textLayerDynamicStyleCount())};
         setTextLayerInstance(Containers::pointer<TextLayerGL>(createLayer(), state.textLayerShared));
 
         /* Create a local font plugin manager if external wasn't passed. If the
