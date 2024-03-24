c374a45e3560636f99bc16eae0065e806bb6ab68
Vladimír Vondruš <mosra@centrum.cz>
2024-03-24 18:05:41 +0100

    Whee: rework BaseLayer and TextLayer style assignment.
    Requiring the calling code to have a compile-time-sized struct felt like
    a good idea in theory. In practice it's absolutely horrendous tho, as:
    
     - The calling code then has to ensure matching order between a style
       enum and a style struct, which is extremely hard to maintain without
       a nasty preprocessor magic.
     - Defining a builtin style means either having to define & document a
       struct that glues a LayerStyleCommon instance together with several
       LayerStyleItem instances, which is nasty on its own, and then have to
       *somehow* populate it. Which in a (C++11) constexpr context means
       either using the implicit initializer, again losing any mapping from
       the actual enum values to the order (thus gaining *nothing* from such
       a "named tuple" definition), or having to give up on any constexpr
       use and assign to the named fields in order to ensure at least some
       ordering sanity.
     - It's extremely annoying and / or impossible to extend the style
       definition for custom widgets -- one has to *derive a struct* for
       that, and then somehow copy the original builtin data to its prefix.
     - The error message when the layer style count differs from the actual
       passed data is total garbage. Nobody is interested in how many bytes
       something occupies, they want to know *what is wrong* and the API
       isn't capable of saying or even knowing that.
    
    So now it's instead a setStyle() API taking the LayerStyleCommon
    instance and a (contiguous) list of LayerStyleItem. A downside is that
    the (GL) implementation then does two GL buffer uploads, alternatively
    it could put them together and upload at once (which means a nasty temp
    allocation). With Vulkan (and I hope WebGPU) this won't be a problem as
    there's a way to submit multiple uploads in a single command, or just
    memory-mapping the buffer and doing a copy directly.
    
    Extending a style is then a matter of creating an array that's larger
    than the compile-time constant, having the custom style items start from
    that constant, and copying the original data to prefix of that array.
    Simple.
    
    For the TextLayer this also merges the previous setStyle() and
    setStyleFonts() APIs together, because they were always meant to be
    called together anyway. It makes the whole usage a lot less janky and
    unclear.

--- src/Magnum/Whee/BaseLayer.h
+++ src/Magnum/Whee/BaseLayer.h
@@ -40,7 +40,7 @@ namespace Magnum { namespace Whee {
 @brief Common style properties for all @ref BaseLayer data
 @m_since_latest
 
-@see @ref BaseLayerStyleItem, @ref BaseLayerGL::Shared::setStyle()
+@see @ref BaseLayerStyleItem, @ref BaseLayer::Shared::setStyle()
 */
 struct BaseLayerStyleCommon {
     /** @brief Construct with default values */
@@ -116,7 +116,7 @@ struct BaseLayerStyleCommon {
 @brief Varying style properties for @ref BaseLayer data
 @m_since_latest
 
-@see @ref BaseLayerStyleCommon, @ref BaseLayerGL::Shared::setStyle()
+@see @ref BaseLayerStyleCommon, @ref BaseLayer::Shared::setStyle()
 */
 struct BaseLayerStyleItem {
     /** @brief Construct with default values */
@@ -663,10 +663,41 @@ class MAGNUM_WHEE_EXPORT BaseLayer: public AbstractVisualLayer {
 /**
 @brief Shared state for the base layer
 
-You'll most likely instantiate the class through @ref BaseLayerGL::Shared,
-which contains a concrete OpenGL implementation.
+Contains style data. You'll most likely instantiate the class through
+@ref BaseLayerGL::Shared. In order to draw the layer it's expected that
+@ref setStyle() was called.
 */
 class MAGNUM_WHEE_EXPORT BaseLayer::Shared: public AbstractVisualLayer::Shared {
+    public:
+        /**
+         * @brief Set style data
+         * @param common        Data common to all styles
+         * @param items         Individual style data
+         * @return Reference to self (for method chaining)
+         *
+         * The @p items view is expected to have the same size as
+         * @ref styleCount().
+         */
+        Shared& setStyle(const BaseLayerStyleCommon& common, Containers::ArrayView<const BaseLayerStyleItem> items);
+        /** @overload */
+        Shared& setStyle(const BaseLayerStyleCommon& common, std::initializer_list<BaseLayerStyleItem> items);
+
+        /* Overloads to remove a WTF factor from method chaining order */
+        #ifndef DOXYGEN_GENERATING_OUTPUT
+        Shared& setStyleTransition(UnsignedInt(*toPressedBlur)(UnsignedInt), UnsignedInt(*toPressedHover)(UnsignedInt), UnsignedInt(*toInactiveBlur)(UnsignedInt), UnsignedInt(*toInactiveHover)(UnsignedInt)) {
+            return static_cast<Shared&>(AbstractVisualLayer::Shared::setStyleTransition(toPressedBlur, toPressedHover, toInactiveBlur, toInactiveHover));
+        }
+        template<class StyleIndex, StyleIndex(*toPressedBlur)(StyleIndex), StyleIndex(*toPressedHover)(StyleIndex), StyleIndex(*toInactiveBlur)(StyleIndex), StyleIndex(*toInactiveHover)(StyleIndex)> Shared& setStyleTransition() {
+            return static_cast<Shared&>(AbstractVisualLayer::Shared::setStyleTransition<StyleIndex, toPressedBlur, toPressedHover, toInactiveBlur, toInactiveHover>());
+        }
+        Shared& setStyleTransition(UnsignedInt(*toPressed)(UnsignedInt), UnsignedInt(*toInactive)(UnsignedInt)) {
+            return static_cast<Shared&>(AbstractVisualLayer::Shared::setStyleTransition(toPressed, toInactive));
+        }
+        template<class StyleIndex, StyleIndex(*toPressed)(StyleIndex), StyleIndex(*toInactive)(StyleIndex)> Shared& setStyleTransition() {
+            return static_cast<Shared&>(AbstractVisualLayer::Shared::setStyleTransition<StyleIndex, toPressed, toInactive>());
+        }
+        #endif
+
     #ifdef DOXYGEN_GENERATING_OUTPUT
     private:
     #else
@@ -679,6 +710,10 @@ class MAGNUM_WHEE_EXPORT BaseLayer::Shared: public AbstractVisualLayer::Shared {
         explicit Shared(UnsignedInt styleCount);
         /* Can't be MAGNUM_WHEE_LOCAL, used by tests */
         explicit Shared(NoCreateT) noexcept;
+
+    private:
+        /* The items are guaranteed to have the same size as styleCount() */
+        virtual void doSetStyle(const BaseLayerStyleCommon& common, Containers::ArrayView<const BaseLayerStyleItem> items) = 0;
 };
 
 }}
