fdd335a5dbaa9ece7ce6932f105caa889d1cc468
Vladimír Vondruš <mosra@centrum.cz>
2024-04-24 17:31:16 -0700

    Whee: pass all compositing rects to doUpdate().
    Similarly as all draw data are passed to doUpdate() first, in order to
    have the update function calculate / upload whatever it needs to upload
    and doesn't have to do that right before submitting the actual
    compositing operation.

--- src/Magnum/Whee/TextLayer.h
+++ src/Magnum/Whee/TextLayer.h
@@ -872,7 +872,7 @@ class MAGNUM_WHEE_EXPORT TextLayer: public AbstractVisualLayer {
         LayerFeatures doFeatures() const override;
 
         LayerStates doState() const override;
-        void doUpdate(LayerStates states, const Containers::StridedArrayView1D<const UnsignedInt>& dataIds, const Containers::StridedArrayView1D<const UnsignedInt>& clipRectIds, const Containers::StridedArrayView1D<const UnsignedInt>& clipRectDataCounts, const Containers::StridedArrayView1D<const Vector2>& nodeOffsets, const Containers::StridedArrayView1D<const Vector2>& nodeSizes, Containers::BitArrayView nodesEnabled, const Containers::StridedArrayView1D<const Vector2>& clipRectOffsets, const Containers::StridedArrayView1D<const Vector2>& clipRectSizes) override;
+        void doUpdate(LayerStates states, const Containers::StridedArrayView1D<const UnsignedInt>& dataIds, const Containers::StridedArrayView1D<const UnsignedInt>& clipRectIds, const Containers::StridedArrayView1D<const UnsignedInt>& clipRectDataCounts, const Containers::StridedArrayView1D<const Vector2>& nodeOffsets, const Containers::StridedArrayView1D<const Vector2>& nodeSizes, Containers::BitArrayView nodesEnabled, const Containers::StridedArrayView1D<const Vector2>& clipRectOffsets, const Containers::StridedArrayView1D<const Vector2>& clipRectSizes, const Containers::StridedArrayView1D<const Vector2>& compositeRectOffsets, const Containers::StridedArrayView1D<const Vector2>& compositeRectSizes) override;
 
     private:
         MAGNUM_WHEE_LOCAL DataHandle createInternal(NodeHandle node);
