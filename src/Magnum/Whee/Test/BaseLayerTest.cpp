3f4c5208bfdcd6d00d75f6c0a2e7e7b894cc36ae
Vladimír Vondruš <mosra@centrum.cz>
2024-06-20 19:08:46 +0200

    Whee: add BaseLayer::Shared::Flag::TextureMask.
    Whem enabled, it causes outline and background blur to be affected by
    texture alpha.

--- src/Magnum/Whee/Test/BaseLayerTest.cpp
+++ src/Magnum/Whee/Test/BaseLayerTest.cpp
@@ -69,6 +69,7 @@ struct BaseLayerTest: TestSuite::Tester {
 
     void sharedDebugFlag();
     void sharedDebugFlags();
+    void sharedDebugFlagSupersets();
 
     void sharedConfigurationConstruct();
     void sharedConfigurationConstructSameStyleUniformCount();
@@ -314,6 +315,7 @@ BaseLayerTest::BaseLayerTest() {
 
               &BaseLayerTest::sharedDebugFlag,
               &BaseLayerTest::sharedDebugFlags,
+              &BaseLayerTest::sharedDebugFlagSupersets,
 
               &BaseLayerTest::sharedConfigurationConstruct,
               &BaseLayerTest::sharedConfigurationConstructSameStyleUniformCount,
@@ -732,6 +734,15 @@ void BaseLayerTest::sharedDebugFlags() {
     std::ostringstream out;
     Debug{&out} << (BaseLayer::Shared::Flag::BackgroundBlur|BaseLayer::Shared::Flag(0xb0)) << BaseLayer::Shared::Flags{};
     CORRADE_COMPARE(out.str(), "Whee::BaseLayer::Shared::Flag::BackgroundBlur|Whee::BaseLayer::Shared::Flag(0xb0) Whee::BaseLayer::Shared::Flags{}\n");
+}
+
+void BaseLayerTest::sharedDebugFlagSupersets() {
+    /* TextureMask is a superset of Textured, so only one should get printed */
+    {
+        std::ostringstream out;
+        Debug{&out} << (BaseLayer::Shared::Flag::Textured|BaseLayer::Shared::Flag::TextureMask);
+        CORRADE_COMPARE(out.str(), "Whee::BaseLayer::Shared::Flag::TextureMask\n");
+    }
 }
 
 void BaseLayerTest::sharedConfigurationConstruct() {
