e2d21f023307efe4dc2b16c99ddbe73d64210201
Vladimír Vondruš <mosra@centrum.cz>
2025-07-12 14:06:25 +0200

    package/ci: run Linux desktop GL and GLES tests using llvmpipe.
    Matches the CI changes done in Magnum. SwiftShader is still used on Mac
    GLES3 and on Android, so I can't remove the SKIPs and XFAILs yet, but
    thanks to llvmpipe it's now going to be actually tested instead of just
    being ran through and then skipped right before drawing anything.

--- package/ci/unix-desktop.sh
+++ package/ci/unix-desktop.sh
@@ -26,6 +26,7 @@ cmake .. \
     -DCMAKE_INSTALL_PREFIX=$HOME/deps \
     -DCMAKE_INSTALL_RPATH=$HOME/deps/lib \
     -DCMAKE_BUILD_TYPE=$CONFIGURATION \
+    -DMAGNUM_TARGET_EGL=$TARGET_EGL \
     -DMAGNUM_WITH_AUDIO=OFF \
     -DMAGNUM_WITH_DEBUGTOOLS=ON \
     -DMAGNUM_WITH_MATERIALTOOLS=OFF \
@@ -41,8 +42,26 @@ cmake .. \
     `# Application libraries needed by various Ui tests` \
     -DMAGNUM_WITH_SDL2APPLICATION=ON \
     -DMAGNUM_WITH_GLFWAPPLICATION=ON \
+    `# AnyImageImporter needed if GL texts are enabled (thus, targeting EGL)` \
+    -DMAGNUM_WITH_ANYIMAGEIMPORTER=$TARGET_EGL \
     -DMAGNUM_BUILD_DEPRECATED=$BUILD_DEPRECATED \
     -DMAGNUM_BUILD_STATIC=$BUILD_STATIC \
+    -G Ninja
+ninja install
+cd ../..
+
+# Magnum Plugins. Used only if GL tests are enabled (thus, targeting EGL), not
+# needed otherwise.
+git clone --depth 1 https://github.com/mosra/magnum-plugins.git
+cd magnum-plugins
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DCMAKE_INSTALL_RPATH=$HOME/deps/lib \
+    -DCMAKE_BUILD_TYPE=$CONFIGURATION \
+    -DMAGNUM_WITH_STBIMAGEIMPORTER=$TARGET_EGL \
+    -DMAGNUM_WITH_STBTRUETYPEFONT=$TARGET_EGL \
     -G Ninja
 ninja install
 cd ../..
@@ -68,7 +87,18 @@ export CORRADE_TEST_COLOR=ON
 export ASAN_OPTIONS="color=always"
 export LSAN_OPTIONS="color=always"
 
-ctest -V -E "GLTest|GLBenchmark"
+# Run GL tests if we can use llvmpipe through EGL. (Not benchmarks because I'm
+# not interested in knowing speed of a random software GPU emulation, further
+# offset by inherent randomness of a CI VM.) Enabled only on some builds to
+# make sure the MAGNUM_TARGET_EGL=OFF option is at least compile-tested too.
+if [ "$TARGET_EGL" == "ON" ]; then
+    # Keep in sync with package/archlinux/PKGBUILD and PKGBUILD-coverage
+    ctest -V -E "GLBenchmark|VkTest"
+    MAGNUM_DISABLE_EXTENSIONS="GL_ARB_multi_bind GL_ARB_shading_language_420pack GL_ARB_explicit_uniform_location" ctest -V -R GLTest
+    MAGNUM_DISABLE_EXTENSIONS="GL_ARB_direct_state_access GL_ARB_robustness" ctest -V -R GLTest
+else
+    ctest -V -E "GLTest|GLBenchmark"
+fi
 
 # Test install, after running the tests as for them it shouldn't be needed
 ninja install
