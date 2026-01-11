c2e9fb8cf7f524d4ebac17f6dabd6b6a461dedec
Vladimír Vondruš <mosra@centrum.cz>
2026-01-11 17:37:33 +0100

    package/homebrew: extract the SHA1 from the version instead of duplicating.
    Co-authored-by: Dora "cat" <cat@thenight.club>

--- package/homebrew/magnum-extras.rb
+++ package/homebrew/magnum-extras.rb
@@ -4,7 +4,7 @@ class MagnumExtras < Formula
   # git describe origin/master, except the `v` prefix
   version "2020.06-865-g54669108e2"
   # Clone instead of getting an archive to have tags for version.h generation
-  url "https://github.com/mosra/magnum-extras.git", revision: "54669108e2"
+  url "https://github.com/mosra/magnum-extras.git", revision: version.to_str().rpartition('g')[2]
   head "https://github.com/mosra/magnum-extras.git"
 
   depends_on "cmake" => :build
